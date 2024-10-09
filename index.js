4a8e4f604d5885895c81a11a04aed4e63d20dc04
hwangjunseong <junseong5832@gmail.com>
2024-10-09 23:26:00 +0900

    add icon

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.use((req, res, next) => {
 app.use("/admin", adminRoutes);
 app.use("/shop", shopRoutes);
 app.use("/users", authRoutes);
-
+// app.use("/", home);
 app.get("/500", errorController.get500);
 app.use(errorController.get404);
 mongoose
